8edb89897dbab9885939e5ca7b703d5afd909eff
Aleksandra Pawlak <synowa8@gmail.com>
2020-04-27 23:11:40 +0200

    CalcEngine with accessors and mutators.

---
+++ src/com/synowa/calcengineaccessorsmutators/MathEquation.java
@@ -0,0 +1,40 @@
+package com.synowa.calcengineaccessorsmutators;
+
+public class MathEquation {
+    public double leftVal;
+    public double rightVal;
+    public char opCode;
+    public double result;
+
+    public double getLeftVal() {return leftVal;}
+    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+    public double getRightVal() {return rightVal;}
+    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+    public char getOpCode() {return opCode;}
+    public void setOpCode(char opCode) {this.opCode = opCode;}
+
+
+    public double getResult() { return result;}
+
+    public void execute() {
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;//(it means result = if val2 non equals zero than go ahead and divide val1/val2 otherwise (":") return zero)
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            default:
+                System.out.println("Error - invalid opCode");
+                result = 0.0d;
+                break;
+        }
+
+    }
+}
